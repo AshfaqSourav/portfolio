a651e3336183bd51fb3a10eacb51cf935243fcfc
AshfaqSourav <ashfaqahmed3339@gmail.com>
2023-11-30 01:57:43 +0600

    first_commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings: ["SQA Engineer" , "Frontend Developer" , "UI/UX Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
